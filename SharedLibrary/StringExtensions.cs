3eab8307c1982ebafe8348456f8874ce3b23735b
P-JSL <althsuwpfl@gmail.com>
2023-08-22 21:09:06 +0900

    초기 커밋.

---
+++ SharedLibrary/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace SharedLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool IsValidXmlTag(this string input)
+        {
+            return Regex.IsMatch(input,@"^<([a-z]+)([^<]+)*(?:>(.*)<\/\1>|s+\/>)$");
+        }
+        public static bool IsValidPassword(this string input)
+        {
+            return Regex.IsMatch(input,"^[a-zA-Z0-9_-]{8,}$");
+        }
+
+        public static bool isValidHex(this string input)
+        {
+            //3개 혹은 6개의 유효한 16진수
+            return Regex.IsMatch(input,"^@?([a-fA-F0-9]{3}|[a-fA-f0-9]{6})$");
+        }
+    }
+}
---
+++ UsingFreeviewFeatures/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Versioning;
+using static System.Console;
+
+Doer.DoSomething();
+public interface IWithStaticAbstract
+{
+    [RequiresPreviewFeatures]
+    static abstract void DoSomething();
+}
+
+public class Doer : IWithStaticAbstract
+{
+    [RequiresPreviewFeatures]
+    public static void DoSomething()
+    {
+        WriteLine("I am an implementation of a static abstract method.");
+    }
+}
+
